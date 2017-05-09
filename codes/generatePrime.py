5e88ae6eb475e3a2c97b3c182f6703331afac6a6
johnson wamuyu <jsonmuchiriwamuyu@gmail.com>
2017-05-09 07:11:39 +0300

    added a test case using 20 as a parameter

--- codes/generatePrime.py
+++ codes/generatePrime.py
@@ -1,16 +1,13 @@
-"""Prime numbers module."""
-
-
-def prime_numbers(number):
-    """Function to generate prime numbers within given range."""
+def prime_number(number):
+    """
+    # A prime number is a number divisible by one or itself
+    # Function to generate prime numbers within given range.
+    """
 
     prime_nos = []
 
     if number in (0, 1):
         return "Zero or One cannot be prime numbers."
-
-    if number < 2:
-        return "Not possible to generate prime numbers for integers less than 2."
 
     if type(number) != int:
         return "Only integers are allowed."
@@ -23,3 +20,11 @@ def prime_numbers(number):
             else:
                 prime_nos.append(i)
     return prime_nos
+
+
+"""
+# Call the function using an example of 20 to test whether is working
+# Output:[2, 3, 5, 7, 11, 13, 17, 19]
+"""
+prime = prime_number(20)
+print(prime)
