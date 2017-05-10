133eee96fbd019264d251b54daf1ed0108917cda
johnson wamuyu <jsonmuchiriwamuyu@gmail.com>
2017-05-10 15:57:06 +0300

    Update generatePrime.py

--- codes/generatePrime.py
+++ codes/generatePrime.py
@@ -1,6 +1,6 @@
 def prime_number(number):
     """
-    # Prime number: a is a number divisible by one or itself
+    # A prime number is a number divisible by one or itself
     # Function to generate prime numbers within given range.
     """
 
@@ -10,21 +10,20 @@ def prime_number(number):
         return "Zero or One cannot be prime numbers."
 
     if type(number) != int:
-        return "Only integers are allowed."
+       return "Only integers are allowed."
 
-    for i in range(2, number + 1):
-        if i > 1:
+    for i in range(2, number):
             for x in range(2, i):
                 if (i % x) == 0:
                     break
             else:
                 prime_nos.append(i)
     return prime_nos
-
-
+	
 """
 # Call the function using an example of 20 to test whether is working
 # Output:[2, 3, 5, 7, 11, 13, 17, 19]
 """
-prime = prime_number(20)
+prime = prime_number('20')
 print(prime)
+
